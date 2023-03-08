bd94631286781b433dd59549fbf1cd953b585ac3
root <partzival@partzival.net>
2023-03-08 13:52:08 +0000

    Adding switchman python code for communication (working for commuting pairs)

--- switchman_code/python_part_on_OrangePi/main.py
+++ switchman_code/python_part_on_OrangePi/main.py
@@ -1,14 +1,37 @@
 #!/bin/python3
+import socket
 import serial
-import time
-ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-ser.reset_input_buffer()
-while True:
-    ser.write(b"P1\n")
+
+def client_program():
+    host = "10.0.0.5"  # as both code is running on same pc
+    port = 13000  # socket server port number
+    # Defining serial parameters into ser
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+    ser.reset_input_buffer() # Emptying initial input buffer
+    
+    client_socket = socket.socket()  # instantiate client socket
+    client_socket.connect((host, port))  # connect to the server
+    
+    input_data="XX"
+    
+    while True:
+        
+        input_data = client_socket.recv(1024).decode()  # receive server port order
+        if input_data=="CL":
+            break
+        print("(DEBUG) Data recv from master : "+input_data) 
+        output_data = serial_connection(input_data,ser)
+        print("(DEBUG) Data recv from Arduino : "+output_data)
+        client_socket.send(output_data.encode())
+
+    client_socket.send("EX".encode())
+    client_socket.close()
+    
+
+def serial_connection(port,ser):
+    ser.write((port+"\n").encode('utf-8')) # writing received port into arduino
     line = ser.readline().decode('utf-8').rstrip()
-    print(line)
-    time.sleep(1)
-    ser.write(b"P4\n")
-    line = ser.readline().decode('utf-8').rstrip()
-    print(line)
-    time.sleep(1)
+    return line
+
+if __name__ == '__main__':
+    client_program()
--- switchman_code/python_part_on_OrangePi/test_Serial.py
+++ switchman_code/python_part_on_OrangePi/test_Serial.py
@@ -4,11 +4,8 @@ import time
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
 while True:
-    ser.write(b"P1\n")
-    line = ser.readline().decode('utf-8').rstrip()
-    print(line)
-    time.sleep(1)
-    ser.write(b"P4\n")
+    port = input(' -> ')
+    ser.write((port+"\n").encode('utf-8'))
     line = ser.readline().decode('utf-8').rstrip()
     print(line)
     time.sleep(1)
